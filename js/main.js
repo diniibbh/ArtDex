26899a06f06258b2a9b14a3ddaf9e258ec4e2682
diniibbh <msiblackshark2005@gmail.com>
2024-10-12 15:46:28 +0530

    Donee
    Done

--- js/main.js
+++ js/main.js
@@ -125,14 +125,62 @@ function openModal() {
 const toggleSwitch = document.querySelector('.switch__input');
 
 function switchTheme(e) {
-    if (!(e.target.checked)) {
+	if (!(e.target.checked)) {
 		console.log(" not Checked")
-        document.body.classList.add('dark-mode');
-    } else {
+		document.body.classList.add('dark-mode');
+	} else {
 		console.log(" Checked")
 
-        document.body.classList.remove('dark-mode');
-    }
+		document.body.classList.remove('dark-mode');
+	}
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+
+$(document).ready(function () {
+	$('#navbarDefault').on('show.bs.collapse', function () {
+		console.log('Navbar is expanding');
+	});
+
+	$('#navbarDefault').on('hide.bs.collapse', function () {
+		console.log('Navbar is collapsing');
+	});
+});
+
+
+$(window).on('load', function () {
+	initSmoothScrolling('.block', 'smoothscroll');
+});
+
+function initSmoothScrolling(container, logoAnimation) {
+
+	var sliderWidth = 0;
+	var animationWidth = 0;
+	var sliderHeight = $('>div>div:first-of-type', container).outerHeight(false);
+
+	$('>div>div', container).each(function () {
+		animationWidth += $(this).outerWidth(false);
+	});
+
+	var slidesVisible = $(container).width() / $('>div>div:first-of-type', container).outerWidth(false);
+	slidesVisible = Math.ceil(slidesVisible);
+
+	var slidesNumber = $('>div>div', container).length;
+	var speed = slidesNumber * 2;
+
+	$('>div>div', container).slice(0, slidesVisible).clone().appendTo($('>div', container));
+
+	$('>div>div', container).each(function () {
+		sliderWidth += $(this).outerWidth(false);
+	});
+
+	$('>div', container).css({ 'width': sliderWidth, 'height': sliderHeight });
+
+	$("<style type='text/css'>@keyframes " + animation + " { 0% { margin-left: 0px; } 100% { margin-left: -" + animationWidth + "px; } } " + $('>div>div:first-of-type', container).selector + " { -webkit-animation: " + animation + " " + speed + "s linear infinite; -moz-animation: " + animation + " " + speed + "s linear infinite; -ms-animation: " + animation + " " + speed + "s linear infinite; -o-animation: " + animation + " " + speed + "s linear infinite; animation: " + animation + " " + speed + "s linear infinite; }</style>").appendTo("head");
+
+	var cl = $(container).attr("class");
+	$(container).removeClass(cl).animate({ 'nothing': null }, 1, function () {
+		$(this).addClass(cl);
+	});
+}
